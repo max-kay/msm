53203f0dc9483aaea41812cab32cfb540fda1bca
augerber <aurelgerber@gmail.com>
2026-04-22 18:02:41 +0200

    fixed: now assumes frame of reference in the middle of the cell automatically and only changes values if they are out of bounds w.r. to size/2

--- v3.cpp
+++ v3.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
+using namespace std;
 
-class v3
-{
-public:
+class v3 {
+  public:
     // member variables
     double x, y, z;
 
@@ -11,35 +11,49 @@ public:
     v3(double x, double y, double z) : x(x), y(y), z(z) {}
 
     // Vector-Scalar Operations
-    v3 operator*(const double &num) { return v3(x * num, y * num, z * num); } // Multiplication
-    v3 operator/(const double &num)                                           // Division
+    v3 operator*(const double &num) {
+        return v3(x * num, y * num, z * num);
+    }                               // Multiplication
+    v3 operator/(const double &num) // Division
     {
-        if (num == 0)
-        {
+        if (num == 0) {
             throw std::domain_error("v3 ERROR: vector-scalar division by zero");
         }
         return v3(x / num, y / num, z / num);
     }
 
     // Vector-Vector Operations
-    v3 operator+(const v3 &vector) { return v3(x + vector.x, y + vector.y, z + vector.z); } // Addition
-    v3 operator-(const v3 &vector) { return v3(x - vector.x, y - vector.y, z - vector.z); } // Subtraction
+    v3 operator+(const v3 &vector) {
+        return v3(x + vector.x, y + vector.y, z + vector.z);
+    } // Addition
+    v3 operator-(const v3 &vector) {
+        return v3(x - vector.x, y - vector.y, z - vector.z);
+    } // Subtraction
 
-    double operator*(const v3 &vector) { return double(x * vector.x + y * vector.y + z * vector.z); } // Scalar Product
-    double dot(const v3 &vector) { return double(x * vector.x + y * vector.y + z * vector.z); }       // Scalar Product
+    double operator*(const v3 &vector) {
+        return double(x * vector.x + y * vector.y + z * vector.z);
+    } // Scalar Product
+    double dot(const v3 &vector) {
+        return double(x * vector.x + y * vector.y + z * vector.z);
+    } // Scalar Product
 
     // Self-Targeting Operations
-    double get_length() { return double(std::sqrt(x * x + y * y + z * z)); } // Euclidian Norm
-    v3 get_direction() { return *this / (*this).get_length(); }              // Normalize
-    v3 operator%(const double &size)                                         // Return to Central Probing Volume
+    double get_length() {
+        return double(std::sqrt(x * x + y * y + z * z));
+    } // Euclidian Norm
+    v3 get_direction() { return *this / (*this).get_length(); } // Normalize
+    v3 operator%(const double &size) // Return to Central Probing Volume
     {
-        // set point of reference in the center of cell
-        *this = *this - v3(size / 2, size / 2, size / 2);
+        if ((x < size / 2) || (x > size / 2)) {
+            x -= std::round(x / size) * size;
+        }
+        if ((y < size / 2) || (y > size / 2)) {
+            y -= std::round(y / size) * size;
+        }
+        if ((z < size / 2) || (z > size / 2)) {
+            z -= std::round(z / size) * size;
+        }
 
-        x -= std::round(x / size) * size;
-        y -= std::round(y / size) * size;
-        z -= std::round(z / size) * size;
-
-        return v3(x, y, z);
+        return v3(x,y,z);
     }
 };
