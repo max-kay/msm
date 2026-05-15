0c53f30bdeb644675d54b77cc0e27058ea55021b
Max Krummenacher <maxkrummenacher@bluewin.ch>
2026-05-15 18:48:54 +0200

    Added plotting

--- python/main.py
+++ python/main.py
@@ -1,9 +1,36 @@
+from sys import argv
+import glob
+import json
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+from imageio import v2 as imageio
+
+PI = 3.1415926
 
 
-def plot_ellipsoids(centers, directions, a, c, rve_side_len=1.0):
-    fig = plt.figure(figsize=(10, 8))
+def create_animation(path, fps=10):
+    images = []
+    image_files = sorted(glob.glob(os.path.join(path, "*.png")))
+
+    if not image_files:
+        print("No images found to animate.")
+        return
+
+    output_path = f"{path}.mp4"
+
+    with imageio.get_writer(output_path, fps=fps) as writer:
+        for filename in image_files:
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+    print(f"Animation saved to: {output_path}")
+
+
+def plot_ellipsoids(
+    centers, directions, path, a, c, rve_side_len=1.0, title=None, iteration=0
+):
+    fig = plt.figure(figsize=(10.08, 8))
     ax = fig.add_subplot(111, projection="3d")
 
     # Create base sphere coordinates
@@ -39,25 +66,68 @@ def plot_ellipsoids(centers, directions, a, c, rve_side_len=1.0):
         y_p = rotated_points[1, :].reshape(y.shape) + center[1]
         z_p = rotated_points[2, :].reshape(z.shape) + center[2]
 
-        ax.plot_surface(x_p, y_p, z_p, alpha=1.0, color="blue", edgecolors=None, linewidth=None)
-        start = center + c*target_dir
-        end = center + a*target_dir
-        ax.plot((start[0], end[0]), (start[1], end[1]),(start[2], end[2]), color="red")
-    ax.set_box_aspect((1,1,1))
-    ax.set_xlim(0.0, rve_side_len)
-    ax.set_ylim(0.0, rve_side_len)
-    ax.set_zlim(0.0, rve_side_len)
+        ax.plot_surface(
+            x_p, y_p, z_p, alpha=1.0, color="blue", edgecolors=None, linewidth=None
+        )
+        start = center + c * target_dir
+        end = center + a * target_dir
+        ax.plot((start[0], end[0]), (start[1], end[1]), (start[2], end[2]), color="red")
+    ax.set_box_aspect((1, 1, 1))
+    ax.set_xlim(-0.5 * rve_side_len, 0.5 * rve_side_len)
+    ax.set_ylim(-0.5 * rve_side_len, 0.5 * rve_side_len)
+    ax.set_zlim(-0.5 * rve_side_len, 0.5 * rve_side_len)
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
-    plt.show()
+    fig.suptitle(title)
+    plt.savefig(f"{path}/{iteration:0>8}.png", dpi=100)
+    plt.close(fig)
+
+
+PATH = "../out"
+
+
+def run_on_path(path):
+    with open(path + "/params.json") as file:
+        config = json.load(file)
+    print("\nPlotting run:", path)
+    frames = sorted(
+        [
+            int(os.path.basename(p).removesuffix("_pos.npy"))
+            for p in glob.glob(os.path.join(path, "*_pos.npy"))
+        ]
+    )
+
+    for i in frames:
+        frame_n = f"{i:06d}"
+        p_file = path + "/" + frame_n + "_pos.npy"
+        d_file = path + "/" + frame_n + "_dir.npy"
+        if os.path.exists(d_file):
+            centers = np.load(p_file)
+            directions = np.load(d_file)
+            plot_ellipsoids(
+                centers,
+                directions,
+                path,
+                a=config["longSemiaxesAB"],
+                c=config["shortSemiaxisC"],
+                rve_side_len=config["lengthSimulationCube"],
+                title=f"{(config['simulationTime'] / config['num_frames'] * i * 1000):.1f} ms",
+                iteration=i,
+            )
+    print("\nMaking animation")
+    create_animation(path, fps=10)
 
 
 def main():
-    n = 10
-    centers = np.random.rand(n, 3)
-    directions = np.random.standard_normal((n, 3))  # Random orientations for c-axis
-    plot_ellipsoids(centers, directions, a=0.05, c=0.0125)
+    if len(argv) > 1:
+        for dir in argv[1:]:
+            run_on_path(dir)
+    else:
+        last_dir = sorted(
+            [dir for dir in os.listdir(PATH) if os.path.isdir(f"{PATH}/{dir}")]
+        )[-1]
+        run_on_path(f"{PATH}/{last_dir}")
 
 
 if __name__ == "__main__":
